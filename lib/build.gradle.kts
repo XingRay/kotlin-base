4f1ad3f241cbf310f230eef2f374adcd29aa977c
XingRay <leixing1012@gmail.com>
2024-10-10 16:57:51 +0800

    1. 更新版本号

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -55,7 +55,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.xingray"
             artifactId = "kotlin-base"
-            version = "0.0.5"
+            version = "0.0.6"
 
             from(components["java"])
         }
