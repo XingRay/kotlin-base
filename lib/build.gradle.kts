c8c3a912e3c93040e1e002fa01cbf2b99eefb547
XingRay <leixing1012@gmail.com>
2024-10-10 15:05:36 +0800

    v 0.0.5 1. DateTimeUtil.kt 更名为 TimeUtil.kt

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -55,7 +55,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.xingray"
             artifactId = "kotlin-base"
-            version = "0.0.4"
+            version = "0.0.5"
 
             from(components["java"])
         }
